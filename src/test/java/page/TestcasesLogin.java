62815176b60c3e51c901382d87a47b3dc7bf669e
Hosam Al-Basiony <hosam.el.basiony@gmail.com>
2025-04-11 13:31:37 +0200

    fix

--- src/test/java/page/TestcasesLogin.java
+++ src/test/java/page/TestcasesLogin.java
@@ -10,7 +10,7 @@ import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import pages.login;
+import page_models.login;
 
 import java.time.Duration;
 
--- src/test/java/page_models/login.java
+++ src/test/java/page_models/login.java
@@ -1,4 +1,4 @@
-package pages;
+package page_models;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
