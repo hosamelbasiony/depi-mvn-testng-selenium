a12b76e78abc3f0b5596d86ce5816fe35056f9d7
Hosam Al-Basiony <hosam.el.basiony@gmail.com>
2025-04-11 13:00:51 +0200

    no TestCasesDmo

--- src/test/java/page/TestCasesDmo.java
+++ src/test/java/page/TestCasesDmo.java
@@ -11,20 +11,20 @@ public class TestCasesDmo {
     //     System.out.println("Test started");
     // }
 
-    @Test(priority = 1)
-    public void testCase1() {
-        System.out.println("Test case 1");
-    }
+    // @Test(priority = 1)
+    // public void testCase1() {
+    //     System.out.println("Test case 1");
+    // }
 
-    @Test(priority = 2)
-    public void testCase2() {
-        System.out.println("Test case 2");
-    }
+    // @Test(priority = 2)
+    // public void testCase2() {
+    //     System.out.println("Test case 2");
+    // }
 
-    @Test(priority = 3)
-    public void testCase3() {
-        System.out.println("Test case 3");
-    }
+    // @Test(priority = 3)
+    // public void testCase3() {
+    //     System.out.println("Test case 3");
+    // }
 
     // @AfterMethod
     // public void tearDown() {
